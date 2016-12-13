1ebe12cdf0fd1bf2a9a587bf8de4a757d4a720f2
Florence Wong <florencewong1020@gmail.com>
2016-12-13 12:52:07 -0800

    set up drawing canvas
    added function drawGame() and window.requestAnimationFrame

--- game.js
+++ game.js
@@ -1,4 +1,14 @@
 var canvas = document.getElementById("game");
 var ctx = canvas.getContext("2d");
-ctx.fillStyle = "rgb(200,0,0)";
-ctx.fillRect(0,0,10,10);
+
+function drawGame() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  // random drawing testing
+  ctx.fillStyle = "rgb(" + Math.floor(Math.random()*200) + ",0,0)";
+  ctx.fillRect(0, 0, 10, Math.random()*50);
+
+  window.requestAnimationFrame(drawGame);
+}
+
+window.requestAnimationFrame(drawGame);
